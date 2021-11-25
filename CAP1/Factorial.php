e08bfa6f473b63d8d202336165b651c42d048f48
JMateo <jmateo@cifpfbmoll.eu>
2021-11-25 19:45:41 +0100

    Add files via upload

---
+++ CAP1/Factorial.php
@@ -0,0 +1,9 @@
+<?php
+  function factorial($n){
+      if($n == 1){
+         return 1;
+      } 
+      return $n * factorial($n - 1);
+    }
+echo "El numero factorial de 3 es:" . factorial(3);
+    ?>
