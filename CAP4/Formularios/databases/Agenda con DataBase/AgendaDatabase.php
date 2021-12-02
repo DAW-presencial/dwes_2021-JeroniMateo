675dd776cf5c502c1a93fa5900049c8d27babdd3
Jeroni <jmateo@cifpfbmoll.eu>
2021-12-02 17:51:42 +0100

    Creacion base de datos DB Usuarios

---
+++ CAP4/Formularios/databases/Agenda con DataBase/AgendaDatabase.php
@@ -0,0 +1,11 @@
+<?php 
+
+
+
+
+?>
+
+<form action="">
+    <label>Nombre</label><input type="text" name="nombre" id="nombre">
+    <label>Telefono</label><input type="text" name="telefono" id="telefono">
+</form>
